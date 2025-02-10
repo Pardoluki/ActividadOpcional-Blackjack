efaa09da095e87b9393985319dcd07b657ea632d
Lucas Pardo <lucaspardoparra@gmail.com>
2025-02-10 11:37:52 +0000

    Data changes

--- blackjack/blackjack/obj/Debug/net8.0/blackjack.AssemblyInfo.cs
+++ blackjack/blackjack/obj/Debug/net8.0/blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d7e831a0e6f195582c0cb94a0359757686c81a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041857c241d0827815d0ac0de16d10cfa5e23598")]
 [assembly: System.Reflection.AssemblyProductAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
